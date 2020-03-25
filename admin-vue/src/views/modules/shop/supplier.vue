322dd21f9763359b3d91e58b06450dc45a86ec84
308921027@qq.com <aaasss123>
2020-03-25 16:24:41 +0800

    优化供应商管理功能

--- admin-vue/src/views/modules/shop/supplier.vue
+++ admin-vue/src/views/modules/shop/supplier.vue
@@ -14,27 +14,23 @@
         <el-form-item v-if="$hasPermission('shop:supplier:save')">
           <el-button type="primary" @click="addOrUpdateHandle()">{{ $t('add') }}</el-button>
         </el-form-item>
-        <el-form-item v-if="$hasPermission('shop:supplier:delete')">
-          <el-button type="danger" @click="deleteHandle()">{{ $t('deleteBatch') }}</el-button>
-        </el-form-item>
       </el-form>
       <el-table v-loading="dataListLoading" :data="dataList" border @selection-change="dataListSelectionChangeHandle" @sort-change="dataListSortChangeHandle" style="width: 100%;">
-        <el-table-column type="selection" header-align="center" align="center" width="50"></el-table-column>
         <el-table-column prop="name" label="名称" header-align="center" align="center"></el-table-column>
-        <el-table-column prop="sort" label="排序" header-align="center" align="center" width="100"/>
         <el-table-column prop="imgs" label="图标" header-align="center" align="center" width="100">
           <template slot-scope="scope">
             <el-image v-if="scope.row.imgs" lazy class="table-img" :src="scope.row.imgs.split(',')[0]" @click="imageViewerHandle(scope.row.imgs.split(','))" fit="cover"/>
           </template>
         </el-table-column>
+        <el-table-column prop="sort" label="排序" header-align="center" align="center" width="100"></el-table-column>
         <el-table-column prop="status" label="状态" header-align="center" align="center" width="100">
           <template slot-scope="scope">
             <el-tag v-if="scope.row.status === 0" size="small" type="info">未审核</el-tag>
             <el-tag v-else-if="scope.row.status === 1" size="small" type="success">已审核</el-tag>
           </template>
         </el-table-column>
-        <el-table-column prop="remark" label="备注" header-align="center" align="center"></el-table-column>
-        <el-table-column prop="content" label="内容" header-align="center" align="center"></el-table-column>
+        <el-table-column prop="content" label="简介" header-align="center" align="center" show-overflow-tooltip></el-table-column>
+        <el-table-column prop="remark" label="备注" header-align="center" align="center" show-overflow-tooltip></el-table-column>
         <el-table-column :label="$t('handle')" fixed="right" header-align="center" align="center" width="150">
           <template slot-scope="scope">
             <el-button v-if="$hasPermission('shop:supplier:update')" type="text" size="small" @click="addOrUpdateHandle(scope.row.id)">{{ $t('update') }}</el-button>
@@ -78,8 +74,7 @@ export default {
         getDataListIsPage: true,
         exportURL: '/shop/supplier/export',
         deleteURL: '/shop/supplier/delete',
-        deleteBatchURL: '/shop/supplier/deleteBatch',
-        deleteIsBatch: true
+        deleteIsBatch: false
       },
       dataForm: {
         name: ''
