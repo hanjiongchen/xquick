560bd1791ebd06056d2d66e952b4890e7434e523
Charles <zhangchaoxu@gmail.com>
2020-03-30 17:56:22 +0800

    修改订单服务

--- rocket/admin/src/main/java/co/xquick/modules/shop/ShopConst.java
+++ rocket/admin/src/main/java/co/xquick/modules/shop/ShopConst.java
@@ -8,6 +8,40 @@ package co.xquick.modules.shop;
 public interface ShopConst {
 
     /**
+     * 订单状态类型
+     */
+    enum OrderStatusEnum {
+
+        /**
+         * 详见name
+         */
+        CANCELED_SYS(-10, "系统取消"),
+        CANCELED_USER(-20, "会员取消"),
+        PLACED(0, "已下单"),
+        COMPLETED(100, "已完成");
+
+        private int value;
+        private String name;
+
+        OrderStatusEnum(int value) {
+            this.value = value;
+        }
+
+        OrderStatusEnum(int value, String name) {
+            this.value = value;
+            this.name = name;
+        }
+
+        public int value() {
+            return this.value;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+    }
+
+    /**
      * 进出库记录类型
      */
     enum StockLogTypeEnum {
