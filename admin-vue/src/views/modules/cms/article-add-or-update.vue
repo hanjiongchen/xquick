bbb0005ff218403e9c15aa2614e73fc069ff7c94
Charles <zhangchaoxu@gmail.com>
2020-04-16 14:56:38 +0800

    remove useless code

--- admin-vue/src/views/modules/cms/article-add-or-update.vue
+++ admin-vue/src/views/modules/cms/article-add-or-update.vue
@@ -138,8 +138,6 @@ export default {
         lat: '',
         lng: '',
         address: '',
-        regionCd: '',
-        regionNm: '',
         pubDate: '',
         validFromDate: '',
         validToDate: '',
