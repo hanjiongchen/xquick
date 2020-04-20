588fad1423c135818eb1bce0e0f782d8c8c94cd9
Charles <zhangchaoxu@gmail.com>
2020-04-20 21:39:50 +0800

    优化task显示

--- rocket/admin/src/main/java/com/nb6868/xquick/modules/sched/entity/TaskEntity.java
+++ rocket/admin/src/main/java/com/nb6868/xquick/modules/sched/entity/TaskEntity.java
@@ -11,29 +11,30 @@ import lombok.EqualsAndHashCode;
  * @author Charles zhangchaoxu@gmail.com
  */
 @Data
-@EqualsAndHashCode(callSuper=false)
+@EqualsAndHashCode(callSuper = false)
 @TableName("sched_task")
 public class TaskEntity extends BaseEntity {
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	/**
-	 * spring bean名称
-	 */
-	private String name;
-	/**
-	 * 参数
-	 */
-	private String params;
-	/**
-	 * cron表达式
-	 */
-	private String cron;
-	/**
-	 * 任务状态  0：暂停  1：正常
-	 */
-	private Integer status;
-	/**
-	 * 备注
-	 */
-	private String remark;
+    /**
+     * spring bean名称
+     */
+    private String name;
+    /**
+     * 参数
+     */
+    private String params;
+    /**
+     * cron表达式
+     */
+    private String cron;
+    /**
+     * 任务状态  0：暂停  1：正常
+     */
+    private Integer status;
+    /**
+     * 备注
+     */
+    private String remark;
+
 }
